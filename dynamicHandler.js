68ec77eb0d8707912d18c4dded0d0393a08d2afb
Tanmay S <99489689+tanmaysamanta@users.noreply.github.com>
2022-06-28 18:56:06 +0530

    Refactored dynamic handler

--- dynamicHandler.js
+++ dynamicHandler.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { serveFileContent } = require('./serveFileContent');
 
 const generateTag = (tag, text) => {
   return `<${tag}>${text}</${tag}>`
@@ -7,11 +6,11 @@ const generateTag = (tag, text) => {
 
 const getTime = () => {
   const time = new Date();
-  return `${time.toLocaleString()}`;
+  return time.toLocaleString();
 };
 
-const formatComment = (comments) => {
-  formatedComments = '';
+const formatComments = (comments) => {
+  let formatedComments = '';
   comments.forEach(comment => {
     const text = `${comment.time}  ${comment.name} : ${comment.comment}`
     formatedComments += generateTag('div', text);
@@ -19,10 +18,22 @@ const formatComment = (comments) => {
   return formatedComments;
 };
 
-function updateFile(formatedComments) {
-  let template = fs.readFileSync('./public/template.html', 'utf8');
-  template = template.replace('__COMMENT__', generateTag('div', formatedComments));
-  fs.writeFileSync('./public/guestbook.html', template);
+const getComments = (fileName) => {
+  return JSON.parse(fs.readFileSync(fileName, 'utf8'));
+};
+
+const getFileContent = (fileName, newContent) => {
+  const content = fs.readFileSync(fileName, 'utf8');
+  return content.replace('__COMMENT__', generateTag('div', newContent));
+};
+
+const guestbookHandler = (request, response) => {
+  const comments = getComments('./public/comments.json');
+  const formatedComments = formatComments(comments);
+  const content = getFileContent('./public/template.html', formatedComments);
+  response.setHeaders('content-type', 'text/html')
+  response.send(content);
+  return true;
 };
 
 const commentHandler = (request, response) => {
@@ -30,25 +41,26 @@ const commentHandler = (request, response) => {
   const currentTime = getTime();
   params.time = currentTime;
 
-  const comments = JSON.parse(fs.readFileSync('./public/comments.json', 'utf8'));
+  const comments = getComments('./public/comments.json');
   comments.unshift(params);
 
   const content = JSON.stringify(comments);
   fs.writeFileSync('./public/comments.json', content);
 
-  const formatedComments = formatComment(comments);
-  updateFile(formatedComments);
+  const formatedComments = formatComments(comments);
+  const fileContent = getFileContent('./public/template.html', formatedComments);
 
-  response.setHeaders('location', '/guestbook.html');
+  response.setHeaders('location', '/guestbook');
+  response.setHeaders('content-type', 'text/html')
   response.statusCode = 301;
-  response.send('');
+  response.send(fileContent);
   return true;
 };
 
 const dynamicHandler = (request, response) => {
   const { uri } = request;
-  if (uri === '/guestbook.html') {
-    return serveFileContent(request, response);
+  if (uri === '/guestbook') {
+    return guestbookHandler(request, response);
   }
   if (uri === '/addcomment') {
     return commentHandler(request, response);
